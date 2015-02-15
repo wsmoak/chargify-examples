ae5f1df0e485784dd05dcda60aebbfa047da0bcc
Wendy Smoak <wsmoak@gmail.com>
2015-02-15 18:18:55 -0500

    Testing changes to chargify_api_ares for issue #100

--- ruby/product-change.rb
+++ ruby/product-change.rb
@@ -1,3 +1,8 @@
+# Testing changes to chargify_api_ares gem for 
+# https://github.com/chargify/chargify_api_ares/issues/100
+
+# Use my locally built preview of the next version
+gem 'chargify_api_ares', '=1.3.2.pre'
 require 'chargify_api_ares'
 
 Chargify.configure do |c|
@@ -5,16 +10,26 @@ Chargify.configure do |c|
   c.api_key   = ENV['CHARGIFY_API_KEY']
 end
 
-# Retrieve a list of all your subscriptions
+# Retrieve a  subscription
 subscription = Chargify::Subscription.find(7570777)
 
 #puts subscription.inspect
 
+puts "Before"
 puts subscription.product.handle
 
-subscription.product.handle = "annual-plan"
+# Un-comment one of the following four lines:
 
+#subscription.product_change('monthly-plan')
+
+#subscription.product_change('annual-plan')
+
+#subscription.delayed_product_change('monthly-plan')
+
+#subscription.cancel_delayed_product_change
+
+# Re-retrieve the subscription
+subscription = Chargify::Subscription.find(7570777)
+
+puts "After"
 puts subscription.product.handle
-
-subscription.save
-
