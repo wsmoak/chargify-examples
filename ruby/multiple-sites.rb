c6a039fc287f6b7b214522bc9ac0608229374082
Wendy Smoak <wsmoak@gmail.com>
2015-03-26 15:37:51 -0400

    Testing reconfiguring the gem to connect to multiple sites in a single app

---
+++ ruby/multiple-sites.rb
@@ -0,0 +1,30 @@
+# Testing multiple sites in single app with chargify_api_ares gem
+
+gem 'chargify_api_ares', '=1.3.2'
+require 'chargify_api_ares'
+
+Chargify.configure do |c|
+  c.subdomain = ENV['CHARGIFY_SUBDOMAIN']
+  c.api_key   = ENV['CHARGIFY_API_KEY']
+end
+
+# Read a  subscription
+sub1 = subscription = Chargify::Subscription.find(7570777)
+puts sub1.id
+
+# Switch to a different subdomain
+Chargify.configure do |c|
+  c.subdomain = ENV['CHARGIFY_OTHER_SUBDOMAIN']
+  c.api_key   = ENV['CHARGIFY_API_KEY']
+end
+
+# And read a different subscription
+
+sub2 = subscription = Chargify::Subscription.find(7570777)
+puts sub2.id
+
+sub3 = subscription = Chargify::Subscription.find(7617516)
+puts sub3.id
+
+# Result:  7570777, 7570777, ERROR 404 Not Found
+# Conclusion:  You can only configure the gem once
