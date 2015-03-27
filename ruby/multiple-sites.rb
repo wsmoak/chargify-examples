afd0971745fb5103075fb606f634b3bd0184f015
Wendy Smoak <wsmoak@gmail.com>
2015-03-27 16:48:45 -0400

    Update to gem version 1.3.3 containing the fix for reconfiguring the subdomain

--- ruby/multiple-sites.rb
+++ ruby/multiple-sites.rb
@@ -1,6 +1,6 @@
 # Testing multiple sites in single app with chargify_api_ares gem
 
-gem 'chargify_api_ares', '=1.3.2'
+gem 'chargify_api_ares', '=1.3.3'
 require 'chargify_api_ares'
 
 Chargify.configure do |c|
@@ -13,16 +13,13 @@ sub1 = subscription = Chargify::Subscription.find(7570777)
 puts sub1.id
 
 # Switch to a different subdomain
-# Note that the 'site' attribute needs to be cleared out before setting a new subdomain
 
 Chargify.configure do |c|
-  c.site = nil
   c.subdomain = ENV['CHARGIFY_OTHER_SUBDOMAIN']
   c.api_key   = ENV['CHARGIFY_API_KEY']
 end
 
 # And read a subscription from the new subdomain
-
 sub2 = subscription = Chargify::Subscription.find(7617516)
 puts sub2.id
 
