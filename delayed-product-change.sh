208ae4460f16de69b23fafaba3d739f70946b8e4
Wendy Smoak <wsmoak@gmail.com>
2015-04-09 13:34:43 -0400

    Add example of delayed product change.

---
+++ delayed-product-change.sh
@@ -0,0 +1,7 @@
+# Testing product change
+# https://docs.chargify.com/api-subscriptions
+
+export SUBSCRIPTION_ID=7570777
+
+curl -u $CHARGIFY_API_KEY:x -H Accept:application/json -H Content-Type:application/json -X PUT -d @delayed-product-change.json https://$CHARGIFY_SUBDOMAIN.chargify.com/subscriptions/$SUBSCRIPTION_ID.json
+
