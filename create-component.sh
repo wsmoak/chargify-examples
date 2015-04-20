aae101d8ac34d358d8e3a88d81cfd2435ecba123
Wendy Smoak <wsmoak@gmail.com>
2015-04-19 20:02:53 -0400

    Update create-component.sh with whitespace

--- create-component.sh
+++ create-component.sh
@@ -5,3 +5,4 @@ export PRODUCT_FAMILY_ID=460852
 
 curl --verbose -u $CHARGIFY_API_KEY:x -H Accept:application/json -H Content-Type:application/json -X POST \
 -d @create-component.json https://$CHARGIFY_SUBDOMAIN.chargify.com/product_families/$PRODUCT_FAMILY_ID/metered_components.json
+
