a78230f328b2a2c4476d1511eb78b710e045cd28
Wendy Smoak <wsmoak@gmail.com>
2015-07-12 09:53:10 -0400

    Add examples of v2 signups

--- signup-apiv2.sh
+++ signup-apiv2.sh
@@ -1,2 +1,6 @@
 curl -u $CHARGIFY_DIRECT_API_ID:$CHARGIFY_DIRECT_PASSWORD -H Accept:application/json -H Content-Type:application/json -X POST -d @signup-apiv2-with-uniqueness-token.json https://api.chargify.com/api/v2/signups
 
+curl -u $CHARGIFY_DIRECT_API_ID:$CHARGIFY_DIRECT_PASSWORD -H Accept:application/json -H Content-Type:application/json -X POST -d @signup-apiv2.json https://api.chargify.com/api/v2/signups
+
+#curl -u $CHARGIFY_DIRECT_API_ID:$CHARGIFY_DIRECT_PASSWORD -H Accept:application/json -H Content-Type:application/json -X POST -d @signup-apiv2-with-lowercase-coupon-code.json https://api.chargify.com/api/v2/signups
+# works fine with lowercase coupon code
