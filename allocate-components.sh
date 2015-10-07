ded00a9923293c54d8937a84e319f0e0cd6aaeb4
Wendy Smoak <wsmoak@gmail.com>
2015-10-07 12:35:11 -0400

    Update test of multiple allocations

--- allocate-components.sh
+++ allocate-components.sh
@@ -1,7 +1,6 @@
-# Testing multiple component allocations to see the payment info in the response
+# Testing multiple component allocations
 # https://docs.chargify.com/api-allocations
 
-export SUBSCRIPTION_ID=8840095
-export COMPONENT_ID=79453
+export SUBSCRIPTION_ID=10202036
 
 curl --verbose -u $CHARGIFY_API_KEY:x -H Accept:application/json -H Content-Type:application/json -X POST -d @allocate-components.json https://$CHARGIFY_SUBDOMAIN.chargify.com/subscriptions/$SUBSCRIPTION_ID/allocations.json
