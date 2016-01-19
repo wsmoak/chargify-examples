c11a065b45c11fbf560fe7f3623beb711dcff329
Wendy Smoak <wsmoak@gmail.com>
2016-01-19 09:22:55 -0500

    Update to latest gem and good sub id

--- ruby/subscription-card-update.rb
+++ ruby/subscription-card-update.rb
@@ -1,5 +1,5 @@
 
-gem 'chargify_api_ares', '=1.3.5'
+gem 'chargify_api_ares', '=1.4.3'
 require 'chargify_api_ares'
 
 Chargify.configure do |c|
@@ -7,11 +7,10 @@ Chargify.configure do |c|
   c.api_key   = ENV['CHARGIFY_API_KEY']
 end
 
-sub = Chargify::Subscription.find 9970657
+sub = Chargify::Subscription.find 11335477
 
-sub.credit_card_attributes = {:full_number => "3", :expiration_year => "2020"}
+sub.credit_card_attributes = {:full_number => "4242424242424242", :expiration_year => "2020"}
 
 sub.save
 
 puts sub.inspect
-
