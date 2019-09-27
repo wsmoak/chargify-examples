95056ea41b1d9818cfed60115e31b71adebca7a5
Wendy Smoak <wsmoak@gmail.com>
2019-09-27 14:17:35 -0400

    Update paypal nonce example

--- create-subscription-with-paypal-nonce.sh
+++ create-subscription-with-paypal-nonce.sh
@@ -1,5 +1,8 @@
 # Create a subscription with a PayPal nonce
 
+# For example nonce values, see
+# https://developers.braintreepayments.com/reference/general/testing/ruby#nonces-representing-alternative-payment-methods
+
 export CHARGIFY_SUBDOMAIN=wsmoak-braintree
 
 curl -u $CHARGIFY_API_KEY:x -H Accept:application/json -H Content-Type:application/json -X POST -d @create-subscription-with-paypal-nonce.json https://$CHARGIFY_SUBDOMAIN.chargify.com/subscriptions.json
